4725aa00415b25fabbb4472bbdad7ec9d57a164e
diego-nst <diegostanalgo1@gmail.com>
2025-11-20 21:03:55 +0800

    list view for flights

--- MagisAir/FirstApp/views.py
+++ MagisAir/FirstApp/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+from django.views.generic.list import ListView
+
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello Django")
+
+class FlightsListView(ListView):
+    '''
+    View that lists every available flight
+    '''
+
+    model = Flight
+
+    
