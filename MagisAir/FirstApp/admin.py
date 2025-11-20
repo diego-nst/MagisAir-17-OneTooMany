d0000bcdb78a7c2486a9868909a7d0c725cb07ff
diego-nst <diegostanalgo1@gmail.com>
2025-11-20 22:04:39 +0800

    added admin control

--- MagisAir/FirstApp/admin.py
+++ MagisAir/FirstApp/admin.py
@@ -1,3 +1,76 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class RouteAdmin(admin.ModelAdmin):
+    model = Route
+
+
+class CityInLine(admin.TabularInline):
+    model = City
+
+
+class CityAdmin(admin.ModelAdmin):
+    model = City
+
+
+class PassengerAdmin(admin.ModelAdmin):
+    model = Passenger
+
+
+class PassengerInLine(admin.TabularInline):
+    model = Passenger
+
+
+class ItemAdmin(admin.ModelAdmin):
+    model = Item
+
+
+class ItemInLine(admin.TabularInline):
+    model = Item
+
+
+class ItineraryAdmin(admin.ModelAdmin):
+    model = Itinerary
+
+
+class ItineraryInLine(admin.TabularInline):
+    model = Itinerary
+
+
+class AssignmentAdmin(admin.ModelAdmin):
+    model = Assignment
+
+
+class AssignmentInLine(admin.TabularInline):
+    model = Assignment
+
+
+class FlightAdmin(admin.ModelAdmin):
+    model = Flight
+
+    inlines = [ItineraryInLine, AssignmentInLine]
+
+
+class BookingAdmin(admin.ModelAdmin):
+    model = Booking
+
+    inlines = [ItemInLine, ItineraryInLine]
+
+
+class CrewAdmin(admin.ModelAdmin):
+    model = Crew
+    
+    inlines = [AssignmentInLine,]
+
+
+
+
+admin.site.register(City, CityAdmin)
+admin.site.register(Route, RouteAdmin)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
+admin.site.register(Booking, BookingAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Crew, CrewAdmin)
+admin.site.register(Assignment, AssignmentAdmin)
+admin.site.register(Itinerary, ItineraryAdmin)
