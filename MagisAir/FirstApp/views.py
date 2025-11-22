af654cbb1a9eb98b044400e29ccb9297fe8b8a2f
diego-nst <diegostanalgo1@gmail.com>
2025-11-23 01:17:22 +0800

    added all possible fields to a search

--- MagisAir/FirstApp/views.py
+++ MagisAir/FirstApp/views.py
@@ -20,8 +20,9 @@ class FlightsListView(ListView):
         ctx = super().get_context_data(**kwargs)
         originSearch = self.request.GET.get('origin')
         destinationSearch = self.request.GET.get('destination')
+        date_min_search = self.request.GET.get('date_min')
+        date_max_search = self.request.GET.get('date_max')        
         results = set()
-        searched = False
         if originSearch and originSearch != "":
             for flight in Flight.objects.filter(route__origin__city_name__contains=originSearch):
                 results.add(flight)
@@ -31,8 +32,22 @@ class FlightsListView(ListView):
             for flight in Flight.objects.filter(route__destination__city_name__contains=destinationSearch):
                 if(flight in temp_results or not temp_results):
                     results.add(flight)
+        temp_results = results
+        results = set()
+        if date_min_search != "" and date_max_search != "":
+            for flight in Flight.objects.filter(departure__gte=date_min_search,departure__lte=date_max_search):
+                if(flight in temp_results or not temp_results):
+                    results.add(flight)
+        elif date_min_search != "":
+            for flight in Flight.objects.filter(departure__gte=date_min_search):
+                if(flight in temp_results or not temp_results):
+                    results.add(flight)
+        elif date_max_search != "":
+            for flight in Flight.objects.filter(departure__lte=date_max_search):
+                if(flight in temp_results or not temp_results):
+                    results.add(flight)
         ctx['results'] = results
-        ctx['searched'] = originSearch != "" or destinationSearch != ""
+        ctx['searched'] = originSearch != "" or destinationSearch != "" or date_min_search != "" or date_max_search != ""
         ctx['flights'] = Flight.objects.all()
         return ctx
 
