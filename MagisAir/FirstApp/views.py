a543bf415718b34e1caa2511aced4df567eeaa8a
diego-nst <diegostanalgo1@gmail.com>
2025-11-23 01:02:31 +0800

    Searching with multiple attributes

--- MagisAir/FirstApp/views.py
+++ MagisAir/FirstApp/views.py
@@ -18,9 +18,21 @@ class FlightsListView(ListView):
     
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        originSearch = self.request.GET.get('search')
-        if originSearch:
-            ctx['results'] = Flight.objects.filter(route__origin__city_name__contains=originSearch)
+        originSearch = self.request.GET.get('origin')
+        destinationSearch = self.request.GET.get('destination')
+        results = set()
+        searched = False
+        if originSearch and originSearch != "":
+            for flight in Flight.objects.filter(route__origin__city_name__contains=originSearch):
+                results.add(flight)
+        if destinationSearch and destinationSearch != "":
+            temp_results = results
+            results = set()
+            for flight in Flight.objects.filter(route__destination__city_name__contains=destinationSearch):
+                if(flight in temp_results or not temp_results):
+                    results.add(flight)
+        ctx['results'] = results
+        ctx['searched'] = originSearch != "" or destinationSearch != ""
         ctx['flights'] = Flight.objects.all()
         return ctx
 
