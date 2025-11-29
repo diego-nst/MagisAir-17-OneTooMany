dd7095221fe419db02533f90fd7f0536890cb614
diego-nst <diegostanalgo1@gmail.com>
2025-11-29 17:26:49 +0800

    deleted unneeded files

--- MagisAir/FirstApp/urls.py
+++ MagisAir/FirstApp/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from . import views
-from .views import UserFlightsView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('flights',views.FlightsListView.as_view(), name='flights' ),
     path('bookings', views.BookingsListView.as_view(), name='bookings_list'),
-    path('booking/<int:pk>', views.BookingsDetailView.as_view(), name='bookings_detail'),
     path('booking/<int:pk>/pay', views.BookingsUpdateView.as_view(), name='bookings_update'),
-    path('userflights', UserFlightsView.as_view(), name='user_flights'),
 ]
 
 app_name = 'bookings'
